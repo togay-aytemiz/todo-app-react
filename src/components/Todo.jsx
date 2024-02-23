44fc387ac59e8d7e0584bab97157a7c1d9cd310d
togay-aytemiz <togayaytemiz@gmail.com>
2024-02-23 18:44:08 +0300

    fix checkbox

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -3,7 +3,7 @@ import { FaRegTrashAlt } from "react-icons/fa";
 const style = {
   li: `flex justify-between bg-slate-200 p-4 my-2 capitalize rounded`,
   liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize rounded`,
-  row: `flex w-full font-medium`,
+  row: `flex w-full font-medium item-center`,
   text: `ml-2 cursor-pointer`,
   textComplete: `ml-2 cursor-pointer line-through`,
   button: `cursor-pointer flex item-center`,
@@ -18,6 +18,7 @@ const Todo = ({ todo, toggleComplete, deleteTodo }) => {
           type="checkbox"
           checked={completed}
           onChange={() => toggleComplete(todo)}
+          className="w-4"
         />
         <p
           className={!completed ? style.text : style.textComplete}
